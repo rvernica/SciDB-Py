5f232352b5b56389a9a119573b3ecff4d113f4d9
Jake Vanderplas <vanderplas@astro.washington.edu>
2013-10-16 08:36:29 -0700

    BUG: include test suite in setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(name=NAME,
       maintainer_email=MAINTAINER_EMAIL,
       download_url=DOWNLOAD_URL,
       license=LICENSE,
-      packages=['scidbpy'],
+      packages=['scidbpy', 'scidbpy.tests'],
       classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
