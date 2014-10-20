c9ec9930ed2361397ff801d8e72a68991dc7a899
Chris Beaumont <beaumont@hawaii.edu>
2014-10-20 17:17:34 -0400

    Version bump

--- scidbpy/__init__.py
+++ scidbpy/__init__.py
@@ -1,7 +1,7 @@
 # License: Simplified BSD, 2014
 # See LICENSE.txt for more information
 
-__version__ = "14.9.0.dev"
+__version__ = "14.10.0.dev"
 
 from .interface import *
 from .scidbarray import *
