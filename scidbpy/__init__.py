3f9bcde2fb1cc16d9bf0113a513a0c96e3dd3e51
Chris Beaumont <beaumont@hawaii.edu>
2014-11-16 15:43:25 -0500

    Back to development: 14.12

--- scidbpy/__init__.py
+++ scidbpy/__init__.py
@@ -2,7 +2,7 @@
 # See LICENSE.txt for more information
 from __future__ import absolute_import, print_function, division, unicode_literals
 
-__version__ = "14.10.0"
+__version__ = "14.12.0.dev"
 
 from .interface import *
 from .scidbarray import *
