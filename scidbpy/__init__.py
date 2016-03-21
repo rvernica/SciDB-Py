3cb6e5731b692263024d9f8466453bc9bea622c3
Kriti Sen Sharma <ksen@paradigm4.com>
2016-03-21 15:20:16 -0400

    Back to development

--- scidbpy/__init__.py
+++ scidbpy/__init__.py
@@ -2,7 +2,7 @@
 # See LICENSE.txt for more information
 from __future__ import absolute_import, print_function, division, unicode_literals
 
-__version__ = "15.12.0"
+__version__ = "15.12.dev"
 
 from .interface import *
 from .scidbarray import *
