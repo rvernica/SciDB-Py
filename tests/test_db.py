65ec31150718939da0df42a997d1357fe2c6f1ba
Rares Vernica <rvernica@gmail.com>
2017-05-04 20:20:56 -0700

    Extend build test with types, index, attributes, schema

--- tests/test_db.py
+++ tests/test_db.py
@@ -27,37 +27,152 @@ class TestDB:
         assert db.iquery(query) == None
         assert type(db.iquery(query, fetch=True)) == numpy.ndarray
 
-    @pytest.mark.parametrize('type_name', [
-        '{}int{}'.format(pre, sz)
-        for pre in ('', 'u')
-        for sz in (8, 16, 32, 64)
-    ] + [
-        'bool',
-        'float',
-        'double',
+    @pytest.mark.parametrize(('type_name', 'schema'), [
+        (type_name, schema)
+        for type_name in [
+                '{}int{}'.format(pre, sz)
+                for pre in ('', 'u')
+                for sz in (8, 16, 32, 64)
+        ] + [
+            'bool',
+            'double',
+            'float',
+            'int8',
+            'uint64',
+        ]
+        for schema in [
+                None,
+                'build<val:{}>[i=1:10,10,0]'.format(type_name),
+                '<val:{}>[i=1:10,10,0]'.format(type_name),
+                '<val:{}>[i=1:10]'.format(type_name),
+                '<val:{}>[i]'.format(type_name),
+        ]
     ])
-    def test_fetch_numpy(self, db, type_name):
-        ar = iquery(
-            db,
-            'build(<val:{}>[i=1:10,10,0], random())'.format(type_name),
-            fetch=True)
-        assert ar.shape == (10,)
-        assert ar.ndim == 1
-
-    @pytest.mark.parametrize('type_name', [
-        '{}int{}'.format(pre, sz)
-        for pre in ('', 'u')
-        for sz in (8, 16, 32, 64)
-    ] + [
-        'bool',
-        'float',
-        'double',
+    @pytest.mark.parametrize('atts_only', [
+        True,
+        False,
     ])
-    def test_fetch_dataframe(self, db, type_name):
+    def test_fetch_numpy(self, db, type_name, atts_only, schema):
+        # NumPy array
         ar = iquery(
             db,
             'build(<val:{}>[i=1:10,10,0], random())'.format(type_name),
             fetch=True,
-            as_dataframe=True)
+            atts_only=atts_only,
+            schema=schema)
+        assert ar.shape == (10,)
+        assert ar.ndim == 1
+
+    @pytest.mark.parametrize(('type_name', 'schema'), [
+        (type_name, schema)
+        for type_name in [
+                '{}int{}'.format(pre, sz)
+                for pre in ('', 'u')
+                for sz in (8, 16, 32, 64)
+        ] + [
+            'bool',
+            'double',
+            'float',
+            'int8',
+            'uint64',
+        ]
+        for schema in [
+                None,
+                'build<val:{}>[i=1:10,10,0]'.format(type_name),
+                '<val:{}>[i=1:10,10,0]'.format(type_name),
+                '<val:{}>[i=1:10]'.format(type_name),
+                '<val:{}>[i]'.format(type_name),
+        ]
+    ])
+    @pytest.mark.parametrize('index', [
+        False,
+        None,
+        [],
+    ])
+    def test_fetch_dataframe(self, db, type_name, index, schema):
+        # Pandas DataFrame
+        ar = iquery(
+            db,
+            'build(<val:{}>[i=1:10,10,0], random())'.format(type_name),
+            fetch=True,
+            as_dataframe=True,
+            index=index,
+            schema=schema)
+        assert ar.shape == (10, 2)
+        assert ar.ndim == 2
+
+    @pytest.mark.parametrize(('type_name', 'schema'), [
+        (type_name, schema)
+        for type_name in [
+                '{}int{}'.format(pre, sz)
+                for pre in ('', 'u')
+                for sz in (8, 16, 32, 64)
+        ] + [
+            'bool',
+            'double',
+            'float',
+            'int8',
+            'uint64',
+        ]
+        for schema in [
+                None,
+                'build<val:{}>[i=1:10,10,0]'.format(type_name),
+                '<val:{}>[i=1:10,10,0]'.format(type_name),
+                '<val:{}>[i=1:10]'.format(type_name),
+                '<val:{}>[i]'.format(type_name),
+        ]
+    ])
+    @pytest.mark.parametrize('index', [
+        False,
+        None,
+        [],
+    ])
+    def test_fetch_dataframe_atts(self, db, type_name, index, schema):
+        # Pandas DataFrame
+        ar = iquery(
+            db,
+            'build(<val:{}>[i=1:10,10,0], random())'.format(type_name),
+            fetch=True,
+            atts_only=True,
+            as_dataframe=True,
+            index=index,
+            schema=schema)
+        assert ar.shape == (10, 1)
+        assert ar.ndim == 2
+
+    @pytest.mark.parametrize(('type_name', 'schema'), [
+        (type_name, schema)
+        for type_name in [
+                '{}int{}'.format(pre, sz)
+                for pre in ('', 'u')
+                for sz in (8, 16, 32, 64)
+        ] + [
+            'bool',
+            'double',
+            'float',
+            'int8',
+            'uint64',
+        ]
+        for schema in [
+                None,
+                'build<val:{}>[i=1:10,10,0]'.format(type_name),
+                '<val:{}>[i=1:10,10,0]'.format(type_name),
+                '<val:{}>[i=1:10]'.format(type_name),
+                '<val:{}>[i]'.format(type_name),
+        ]
+    ])
+    @pytest.mark.parametrize('index', [
+        True,
+        ['val'],
+    ])
+    def test_fetch_dataframe_index(self, db, type_name, index, schema):
+        # Pandas DataFrame, index
+        ar = iquery(
+            db,
+            'build(<val:{}>[i=1:10,10,0], random())'.format(type_name),
+            fetch=True,
+            as_dataframe=True,
+            index=index,
+            schema=schema)
         assert ar.shape == (10, 1)
         assert ar.ndim == 2
