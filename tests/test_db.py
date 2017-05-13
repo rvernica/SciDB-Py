1365f25f8820fce3bcbe2f6fbed10a1864b1e356
Rares Vernica <rvernica@gmail.com>
2017-05-13 09:12:10 +0200

    Separate variety tests in a different class

--- tests/test_db.py
+++ tests/test_db.py
@@ -4,6 +4,136 @@ import pytest
 from scidbpy.db import connect, iquery
 from scidbpy.schema import Schema
 
+
+@pytest.fixture(scope='module')
+def db():
+    return connect()
+
+
+class TestDB:
+
+    @pytest.mark.parametrize('args', [
+        {'scidb_auth': ('foo', 'bar')},
+        {'scidb_url': 'http://localhost:8080', 'scidb_auth': ('foo', 'bar')},
+    ])
+    def test_connect_exception(self, args):
+        with pytest.raises(Exception):
+            connect(**args)
+
+    @pytest.mark.parametrize('query', [
+        'list()',
+        "list('operators')",
+    ])
+    def test_iquery(self, db, query):
+        assert db.iquery(query) is None
+        assert type(db.iquery(query, fetch=True)) == numpy.ndarray
+
+    @pytest.mark.parametrize(('type_name', 'schema'), [
+        (type_name, schema)
+        for type_name in [
+                '{}int{}'.format(pre, sz)
+                for pre in ('', 'u')
+                for sz in (8, 16, 32, 64)
+        ] + [
+            'bool',
+            'double',
+            'float',
+        ] + [
+            '{}int{}'.format(u, sz)
+            for u in ('', 'u') for sz in (8, 16, 32, 64)
+        ]
+        for schema in [
+                None,
+                'build<val:{}>[i=1:10,10,0]'.format(type_name),
+                '<val:{}>[i=1:10,10,0]'.format(type_name),
+                '<val:{}>[i=1:10]'.format(type_name),
+                '<val:{}>[i]'.format(type_name),
+        ]
+    ])
+    @pytest.mark.parametrize('atts_only', [
+        True,
+        False,
+    ])
+    def test_fetch_numpy(self, db, type_name, atts_only, schema):
+        # NumPy array
+        ar = iquery(
+            db,
+            'build(<val:{}>[i=1:10,10,0], random())'.format(type_name),
+            fetch=True,
+            atts_only=atts_only,
+            schema=schema)
+        if not atts_only:
+            for i in range(10):
+                assert ar[i][0] == i + 1
+        assert ar.shape == (10,)
+        assert ar.ndim == 1
+
+    @pytest.mark.parametrize(('type_name', 'schema'), [
+        (type_name, schema)
+        for type_name in [
+                '{}int{}'.format(pre, sz)
+                for pre in ('', 'u')
+                for sz in (8, 16, 32, 64)
+        ] + [
+            'bool',
+            'double',
+            'float',
+            'int8',
+            'uint64',
+        ]
+        for schema in [
+                None,
+                'build<val:{}>[i=1:10,10,0]'.format(type_name),
+                '<val:{}>[i=1:10,10,0]'.format(type_name),
+                '<val:{}>[i=1:10]'.format(type_name),
+                '<val:{}>[i]'.format(type_name),
+        ]
+    ])
+    def test_fetch_dataframe(self, db, type_name, schema):
+        # Pandas DataFrame
+        ar = iquery(
+            db,
+            'build(<val:{}>[i=1:10,10,0], random())'.format(type_name),
+            fetch=True,
+            as_dataframe=True,
+            schema=schema)
+        assert ar.shape == (10, 2)
+        assert ar.ndim == 2
+
+    @pytest.mark.parametrize(('type_name', 'schema'), [
+        (type_name, schema)
+        for type_name in [
+                '{}int{}'.format(pre, sz)
+                for pre in ('', 'u')
+                for sz in (8, 16, 32, 64)
+        ] + [
+            'bool',
+            'double',
+            'float',
+            'int8',
+            'uint64',
+        ]
+        for schema in [
+                None,
+                'build<val:{}>[i=1:10,10,0]'.format(type_name),
+                '<val:{}>[i=1:10,10,0]'.format(type_name),
+                '<val:{}>[i=1:10]'.format(type_name),
+                '<val:{}>[i]'.format(type_name),
+        ]
+    ])
+    def test_fetch_dataframe_atts(self, db, type_name, schema):
+        # Pandas DataFrame
+        ar = iquery(
+            db,
+            'build(<val:{}>[i=1:10,10,0], random())'.format(type_name),
+            fetch=True,
+            atts_only=True,
+            as_dataframe=True,
+            schema=schema)
+        assert ar.shape == (10, 1)
+        assert ar.ndim == 2
+
+
 variety_atts_types = """b  :bool       null,
                         c  :char       null,
                         d  :double     null,
@@ -191,11 +321,6 @@ variety_values = numpy.array(
 
 
 @pytest.fixture(scope='module')
-def db():
-    return connect()
-
-
-@pytest.fixture(scope='module')
 def variety(db):
     for q in variety_queries['create']:
         iquery(db, q)
@@ -206,128 +331,7 @@ def variety(db):
         iquery(db, q)
 
 
-class TestDB:
-
-    @pytest.mark.parametrize('args', [
-        {'scidb_auth': ('foo', 'bar')},
-        {'scidb_url': 'http://localhost:8080', 'scidb_auth': ('foo', 'bar')},
-    ])
-    def test_connect_exception(self, args):
-        with pytest.raises(Exception):
-            connect(**args)
-
-    @pytest.mark.parametrize('query', [
-        'list()',
-        "list('operators')",
-    ])
-    def test_iquery(self, db, query):
-        assert db.iquery(query) is None
-        assert type(db.iquery(query, fetch=True)) == numpy.ndarray
-
-    @pytest.mark.parametrize(('type_name', 'schema'), [
-        (type_name, schema)
-        for type_name in [
-                '{}int{}'.format(pre, sz)
-                for pre in ('', 'u')
-                for sz in (8, 16, 32, 64)
-        ] + [
-            'bool',
-            'double',
-            'float',
-        ] + [
-            '{}int{}'.format(u, sz)
-            for u in ('', 'u') for sz in (8, 16, 32, 64)
-        ]
-        for schema in [
-                None,
-                'build<val:{}>[i=1:10,10,0]'.format(type_name),
-                '<val:{}>[i=1:10,10,0]'.format(type_name),
-                '<val:{}>[i=1:10]'.format(type_name),
-                '<val:{}>[i]'.format(type_name),
-        ]
-    ])
-    @pytest.mark.parametrize('atts_only', [
-        True,
-        False,
-    ])
-    def test_fetch_numpy(self, db, type_name, atts_only, schema):
-        # NumPy array
-        ar = iquery(
-            db,
-            'build(<val:{}>[i=1:10,10,0], random())'.format(type_name),
-            fetch=True,
-            atts_only=atts_only,
-            schema=schema)
-        if not atts_only:
-            for i in range(10):
-                assert ar[i][0] == i + 1
-        assert ar.shape == (10,)
-        assert ar.ndim == 1
-
-    @pytest.mark.parametrize(('type_name', 'schema'), [
-        (type_name, schema)
-        for type_name in [
-                '{}int{}'.format(pre, sz)
-                for pre in ('', 'u')
-                for sz in (8, 16, 32, 64)
-        ] + [
-            'bool',
-            'double',
-            'float',
-            'int8',
-            'uint64',
-        ]
-        for schema in [
-                None,
-                'build<val:{}>[i=1:10,10,0]'.format(type_name),
-                '<val:{}>[i=1:10,10,0]'.format(type_name),
-                '<val:{}>[i=1:10]'.format(type_name),
-                '<val:{}>[i]'.format(type_name),
-        ]
-    ])
-    def test_fetch_dataframe(self, db, type_name, schema):
-        # Pandas DataFrame
-        ar = iquery(
-            db,
-            'build(<val:{}>[i=1:10,10,0], random())'.format(type_name),
-            fetch=True,
-            as_dataframe=True,
-            schema=schema)
-        assert ar.shape == (10, 2)
-        assert ar.ndim == 2
-
-    @pytest.mark.parametrize(('type_name', 'schema'), [
-        (type_name, schema)
-        for type_name in [
-                '{}int{}'.format(pre, sz)
-                for pre in ('', 'u')
-                for sz in (8, 16, 32, 64)
-        ] + [
-            'bool',
-            'double',
-            'float',
-            'int8',
-            'uint64',
-        ]
-        for schema in [
-                None,
-                'build<val:{}>[i=1:10,10,0]'.format(type_name),
-                '<val:{}>[i=1:10,10,0]'.format(type_name),
-                '<val:{}>[i=1:10]'.format(type_name),
-                '<val:{}>[i]'.format(type_name),
-        ]
-    ])
-    def test_fetch_dataframe_atts(self, db, type_name, schema):
-        # Pandas DataFrame
-        ar = iquery(
-            db,
-            'build(<val:{}>[i=1:10,10,0], random())'.format(type_name),
-            fetch=True,
-            atts_only=True,
-            as_dataframe=True,
-            schema=schema)
-        assert ar.shape == (10, 1)
-        assert ar.ndim == 2
+class TestVariety:
 
     @pytest.mark.parametrize('schema', [
         None,
