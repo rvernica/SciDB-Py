7bd30de85a94776ebb67f2807c86b884525bc6b1
Rares Vernica <rvernica@gmail.com>
2017-05-21 23:23:29 +0200

    Setup script and README * initial setup.py script * defaults for pytest * README.str file * update Travis to user pytest defaults and install the package

---
+++ README.rst
@@ -0,0 +1,9 @@
+* SciDB-Py: Python Interface to SciDB
+
+[![Build Status](https://travis-ci.org/Paradigm4/SciDB-Py.svg?branch=devel)](https://travis-ci.org/Paradigm4/SciDB-Py)
+
+** Install
+
+```bash
+pip install git+http://github.com/paradigm4/scidb-py.git@devel
+```
---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+import scidbpy
+
+
+NAME = 'scidb-py'
+DESCRIPTION = 'Python library for SciDB'
+AUTHOR = 'Rares Vernica'
+AUTHOR_EMAIL = 'rvernica@gmail.com'
+DOWNLOAD_URL = 'http://github.com/Paradigm4/SciDB-Py'
+LICENSE = 'Simplified BSD'
+VERSION = scidbpy.__version__
+
+setup(name=NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      author=AUTHOR,
+      author_email=AUTHOR_EMAIL,
+      download_url=DOWNLOAD_URL,
+      license=LICENSE,
+      packages=['scidbpy'],
+      install_requires=[
+          'enum34',
+          'numpy',
+          'pandas',
+          'requests',
+      ],
+      )
