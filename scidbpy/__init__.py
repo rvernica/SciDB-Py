717a007c5ec683a50340771ca0b30cee8b7ab481
Rares Vernica <rvernica@gmail.com>
2017-09-09 13:44:53 -0700

    Set version to 16.9.dev0

--- scidbpy/__init__.py
+++ scidbpy/__init__.py
@@ -2,7 +2,7 @@
 # See LICENSE.txt for more information
 from __future__ import absolute_import, print_function, division, unicode_literals
 
-__version__ = "15.12.dev"
+__version__ = "16.9.dev0"
 
 from .interface import *
 from .scidbarray import *
